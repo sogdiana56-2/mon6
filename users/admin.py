65c1f8dc57e97fd5b5694776b97a27afd60e85bd
sogdiana56-2 <sogdiana0608@icloud.com>
2026-01-25 23:20:33 +0600

    first commit

---
+++ users/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+
+    list_display = ("id", "email", "phone_number", "is_active")
+    ordering = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "phone_number", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "phone_number", "password1", "password2"),
+            },
+        ),
+    )
